0810f5dc0bb2036e178b27405cde50691af9e871
limthelimn <nuttawan.lim@gmail.com>
2021-02-02 16:52:18 +0700

    lab14/5

--- lab14_5.cpp
+++ lab14_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,50 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void swap(int &x,int &y){
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+void shuffle(int *a,int *b, int *c, int *d){
+	int N = 5;
+	for(int j = 0;j < N;j++){
+		int k = rand() % 12+1;
+
+		switch(k){
+			case 1 :	swap(*a,*b);
+						break;
+			case 2 :	swap(*a,*c);
+						break;
+			case 3 :	swap(*a,*d);
+						break;
+			case 4 :	swap(*b,*a);
+						break;
+			case 5 :	swap(*b,*c);
+						break;
+			case 6 :	swap(*b,*d);
+						break;
+			case 7 :	swap(*c,*a);
+						break;
+			case 8 :	swap(*c,*b);
+						break;
+			case 9 :	swap(*c,*d);
+						break;
+			case 10 :	swap(*d,*a);
+						break;
+			case 11 :	swap(*d,*b);
+						break;
+			case 12 :	swap(*d,*c);
+						break;
+		}
+	}
+}
+
